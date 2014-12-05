4a6b82288874c199330b055f85e37b2fe86b356c
bjarneo <bjarne.oeverli@gmail.com>
2014-12-05 08:30:54 +0100

    first

--- README.md
+++ README.md
@@ -2,3 +2,28 @@ urldiff
 =======
 
 Python tool using diff and curl to check different content on webpages. Note: This doesn't apply SPAs.
+<br />
+I do recommend installing colordiff (apt-get install colordiff).
+<br />
+This tool is only tested in mint and ubuntu.
+
+Usage:
+```bash
+chmod +x check.py
+./check.py
+```
+
+or
+
+```bash
+python check.py "http://www.web.com/::http://www.web.com/?param=true"
+```
+
+or
+
+```bash
+ln -s /your/folder/urldiff/check.py /usr/local/bin/urldiff
+
+urldiff "url::url"
+```
+
---
+++ check.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+
+import os
+import argparse
+import sys
+import time
+import apt
+
+
+parser = argparse.ArgumentParser(description='Check difference between urls')
+
+parser.add_argument('-u', help='Pass urls. Ex: -u http://url.com?asdasdS:http://url.com')
+
+args = parser.parse_args()
+keys = ['u']
+params = {}
+diff_tool = 'diff'
+cache = apt.Cache()
+script_path = os.path.dirname(os.path.realpath(sys.argv[0]))
+
+for k in keys:
+    if args.__getattribute__(k):
+        params[k] = args.__getattribute__(k)
+
+if len(params) == 0:
+    parser.print_usage()
+    sys.exit()
+
+if cache['colordiff'].is_installed:
+    diff_tool = 'colordiff'
+else:
+    print 'You should install colordiff. apt-get install colordiff\n'
+
+if '|||' in args.u:
+    urls = args.u.split('|||')
+
+    os.system('curl "%s" > %s/page1.log | curl "%s" > %s/page2.log' % (urls[0], script_path, urls[1], script_path))
+
+    os.system('%s %s/page1.log %s/page2.log' % (diff_tool, script_path, script_path))
